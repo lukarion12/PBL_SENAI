8b4cd9b473764fb483e40bc82d35221b6eb7b89b
lukarion12 <lucass1202@hotmail.com.br>
2017-06-26 09:18:55 +0000

    agora foi, olhar banco de dados

--- pblcam/lista_caminhoes.php
+++ pblcam/lista_caminhoes.php
@@ -4,13 +4,38 @@ include("template/header.php");
 ?>
 
 <div class="container menu-espaco">
-    <div class="jumbotron">
-        <h1>Flash Entregas</h1>
-        <p>A entrega mais rápida do Senai.</p>
-        <p>
-            <a class="btn btn-lg btn-primary" href="lista_caminhoes.php" role="button">Listar Caminhões Disponíveis</a>
-        </p>
-    </div>
+
+    <h1> Listagem dos Caminhões e Entregas </h1>
+    
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome Caminhão</th>
+            </tr>
+        </thead>
+        <?php
+            include("item.php");
+        
+            $banco_de_dados = unserialize(file_get_contents("banco_de_dados.db"));
+            $quantidade_itens = count($banco_de_dados);
+            /*
+            for($i=0;$i<$quantidade_itens; $i++){
+                echo "<tr>
+                    <th scope=\"row\">". $i ."</th>
+                    <td>". $banco_de_dados[$i]->nome_cliente ."</td>
+                    <td>". $banco_de_dados[$i]->descricao_produto ."</td>
+                    <td>". $banco_de_dados[$i]->endereco_cliente ."</td>
+                    <td>". $banco_de_dados[$i]->produto ."</td>
+                    <td>". $banco_de_dados[$i]->qtd_produto ."</td>
+                </tr>";
+            }
+            */
+        ?>
+        
+    </table>
+
+    <a href="deletar_tudo.php" class="btn btn-danger"> Deletar todos os itens </a>
 </div>
 
 <?php include("template/footer.php"); ?>
